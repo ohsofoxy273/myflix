8c21a91f34a0eb973f51232a793954e463868bfa
ohsofoxy273 <ohsofoxy273@hotmail.com>
2014-11-08 15:00:27 -0600

    Build the home page - version 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,32 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
+Video.create(title: "Monk",
+						 description: "A funny show",
+						 small_cover_url: "tmp/monk.jpg",
+						 large_cover_url: "tmp/monk_large.jpg")
+
+Video.create(title: "Family guy",
+						 description: "A funny  TV show",
+						 small_cover_url: "tmp/family_guy.jpg",
+						 large_cover_url: "tmp/monk_large.jpg")
+
+Video.create(title: "Futurama",
+						 description: "A funny show",
+						 small_cover_url: "tmp/futurama.jpg",
+						 large_cover_url: "tmp/futurama.jpg")
+
+Video.create(title: "South Park",
+						 description: "A funny cartoon show",
+						 small_cover_url: "tmp/south_park.jpg",
+						 large_cover_url: "tmp/south_park.jpg")
+
+Video.create(title: "Monk",
+						 description: "A funny show",
+						 small_cover_url: "tmp/monk.jpg",
+						 large_cover_url: "tmp/monk_large.jpg")
+
 Video.create(title: "Monk",
 						 description: "A funny show",
 						 small_cover_url: "tmp/monk.jpg",
