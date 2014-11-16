6acf3c9bbac0fd1d52ea7ad76c60c6e5c61cacb9
ohsofoxy273 <ohsofoxy273@hotmail.com>
2014-11-15 18:53:55 -0600

    add form for sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Myflix::Application.routes.draw do
-  resources :users, only: [:new]
+  
+  resources :users
+  resources :sessions, only: :new
 
   root to: "static_pages#home"
   get 'ui(/:action)', controller: 'ui'
