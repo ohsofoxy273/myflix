90c3724522a3359f6a5538db59b7ba6137aee109
ohsofoxy273 <ohsofoxy273@hotmail.com>
2015-01-06 20:43:04 -0600

    Add Figaro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Myflix::Application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'murmuring-chamber-9731.herokuapp.com',
+    :domain         => ENV['PRODUCTION_DOMAIN'],
     :authentication => :plain,
   }
 
